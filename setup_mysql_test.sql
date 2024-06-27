1664a6800da47ca3c3748b3f4067659e5c595639
Lex9jar <alexanderedim89@gmail.com>
2024-06-27 13:16:09 +0100

    Prepares a test MySQL server for the project

---
+++ setup_mysql_test.sql
@@ -0,0 +1,12 @@
+-- Prepare a MySQL server
+
+-- Creates a database if it doesn't exists
+CREATE DATABASE IF NOT EXISTS hbnb_test_db;
+
+-- Creates a new USER if it doesn't exists and grants all privileges of hbnb_test_db
+-- to it.
+CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
+GRANT ALL PRIVILEGES ON hbnb_test_db.* TO 'hbnb_test'@'localhost';
+
+-- Grants the SELECT privilege of performance_scheme to the new user
+GRANT SELECT ON performance_scheme.* TO 'hbnb_test'@'localhost';
