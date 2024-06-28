1685ae384a28f64421f9bef6d4eb5cc6bef255d5
Lex9jar <alexanderedim89@gmail.com>
2024-06-28 16:45:50 +0100

    Modified the 'User' model to support MySQL operations

--- models/user.py
+++ models/user.py
@@ -1,19 +1,21 @@
 #!/usr/bin/python3
 """Defines a class <User."""
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, String
 
 
-class User(BaseModel):
+class User(BaseModel, Base):
     """A <User> class that inherits from <BaseModel>.
 
     Class Public Attributes:
-        email (str): ...
-        password (str): ...
-        first_name (str): ...
-        last_name (str): ...
+        email (str): Email of the user
+        password (str): Password of the user
+        first_name (str): Optional first_name of the user
+        last_name (str): Optional last_name of the user
     """
+    __tablename__ = "users"
 
-    email = str()
-    password = str()
-    first_name = str()
-    last_name = str()
+    email = Column(String(128), nullable=False)
+    password = Column(String(128), nullable=False)
+    first_name = Column(String(128))
+    last_name = Column(String(128))
