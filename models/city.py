96a5224db33cb0ce11aeb391811f387c2a5dbc5b
Lex9jar <alexanderedim89@gmail.com>
2024-06-28 16:15:51 +0100

    Improved the 'City' model to support both 'FileStorage' and 'DBStorage' operation

--- models/city.py
+++ models/city.py
@@ -1,15 +1,19 @@
 #!/usr/bin/python3
 """Defines a class <City>."""
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, String, ForeignKey
+from sqlalchemy.orm import relationship
+import models
 
 
-class City(BaseModel):
+class City(BaseModel, Base):
     """A class <City> that inherits from <BaseModel>.
 
     Public class attributes:
         state_id (str): The <id> of a state.
         name (str): Name of the city.
     """
+    __tablename__ = "cities"
 
-    state_id = str()
-    name = str()
+    name = Column(String(128), nullable=False)
+    state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
