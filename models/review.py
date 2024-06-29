2641989f85461d8ecb5bbbb3e0d404065f283183
Lex9jar <alexanderedim89@gmail.com>
2024-06-29 18:08:07 +0100

    Updated the 'Review' class to support SQLAlchemy operations

--- models/review.py
+++ models/review.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
 """Defines a class <Review>."""
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, String, ForeignKey
 
 
-class Review(BaseModel):
+class Review(BaseModel, Base):
     """A class <Review> that inherits from <BaseModel>.
 
     Public class attributes:
@@ -11,7 +12,7 @@ class Review(BaseModel):
         user_id (str): ...
         text (str): ...
     """
-
-    place_id = str()
-    user_id = str()
-    text = str()
+    __tablename__ = "reviews"
+    text = Column(String(1024), nullable=False)
+    place_id = Column(String(60), ForeignKey("places.id"), nullable=False)
+    user_id = Column(String(60), ForeignKey("users.id"), nullable=False)
