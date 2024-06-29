0f8cdb7e4d590256047e8e7f68b07f70603161a5
Lex9jar <alexanderedim89@gmail.com>
2024-06-29 15:26:55 +0100

    Updated the class 'City' to support SQLAlchmey operation

--- models/city.py
+++ models/city.py
@@ -3,7 +3,7 @@
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String, ForeignKey
 from sqlalchemy.orm import relationship
-import models
+from models.place import Place
 
 
 class City(BaseModel, Base):
@@ -17,3 +17,5 @@ class City(BaseModel, Base):
 
     name = Column(String(128), nullable=False)
     state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
+    places = relationship("Place", cascade="all, delete, delete-orphan",
+                          backref="cities")
