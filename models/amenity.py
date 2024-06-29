d46de69bc945bd3ecb5f1dda7cbb9074f4aa8f64
Lex9jar <alexanderedim89@gmail.com>
2024-06-29 20:38:24 +0100

    Updated the class 'Amenity' to be a table of many to many relationship with 'Place'

--- models/amenity.py
+++ models/amenity.py
@@ -1,13 +1,18 @@
 #!/usr/bin/python3
 """Defines a class <Amenity>."""
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, String
+from sqlalchemy.orm import relationship
+from models.place import place_amenity
 
 
-class Amenity(BaseModel):
+class Amenity(BaseModel, Base):
     """A class <Amenity> that inherits from <BaseModel>.
 
     Public class attribute:
         name (str): ...
     """
+    __tablename__ = "amenities"
 
-    name = str()
+    name = Column(String(128), nullable=False)
+    place_amenities = relationship("Place", secondary=place_amenity)
