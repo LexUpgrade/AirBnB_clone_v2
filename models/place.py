0db92d76ad44a1038158335881360b0863248ead
Lex9jar <alexanderedim89@gmail.com>
2024-06-29 20:39:06 +0100

    Updated the class 'Place' to have a many to many relationship table with 'Amenity'

--- models/place.py
+++ models/place.py
@@ -1,10 +1,19 @@
 #!/usr/bin/python3
 """Defines a class <Place>."""
 from models.base_model import BaseModel, Base
-from sqlalchemy import Column, String, Integer, Float, ForeignKey
+from sqlalchemy import Column, String, Integer, Float, ForeignKey, Table
 from sqlalchemy.orm import relationship
 from os import getenv
 import models
+
+place_amenity = Table("place_amenity", Base.metadata,
+                      Column("place_id", String(60),
+                             ForeignKey("places.id"),
+                             primary_key=True, nullable=False),
+                      Column("amenity_id", String(60),
+                             ForeignKey("amenities.id"),
+                             primary_key=True, nullable=False)
+                      )
 
 
 class Place(BaseModel, Base):
@@ -40,9 +49,24 @@ class Place(BaseModel, Base):
     if getenv("HBNB_TYPE_STORAGE") == "db":
         reviews = relationship("Review", cascade="all, delete, delete-orphan",
                                backref="place")
+        amenities = relationship("Amenity", secondary=place_amenity,
+                                 viewonly=False,
+                                 back_populates="place_amenities")
     else:
         @property
         def reviews(self):
+            """Returns list of review objects with place_id == self.id."""
             objs = models.storage.all("Review")
             result = [v for k, v in objs.items() if v.place_id == self.id]
             return result
+
+        @property
+        def amenities(self):
+            """Returns list of amenity ids."""
+            return self.amenity_ids
+
+        @amenities.setter
+        def amenities(self, obj=None):
+            """Appends amenity ids to self.amenity_ids."""
+            if type(obj) is Amenity and obj.id not in self.amenity_ids:
+                self.amenity_ids.append(obj.id)
