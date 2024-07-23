57e6da547509fda0af77f8b56c046c4ca5f1f567
LexUpgrade <alexanderedim89@gmail.com>
2024-07-23 14:17:19 +0100

    Sets up a web server (Nginx) for deployment of web_static

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,16 +1,35 @@
 #!/usr/bin/env bash
 # Sets up a web server (Nginx) for deployment of web_static.
 
+# Install Nginx if it not already installed.
 apt-get update &&
 	apt-get install -y nginx
 
+# Creates the folder /data/ if it doesn’t already exist
+# Creates the folder /data/web_static/ if it doesn’t already exist
+# Creates the folder /data/web_static/releases/ if it doesn’t already exist
+# Creates the folder /data/web_static/shared/ if it doesn’t already exist
+# Creates the folder /data/web_static/releases/test/ if it doesn’t already exist
 mkdir -p /data/web_static/releases/test/
 mkdir -p /data/web_static/shared/
-echo "Holberton School" > /data/web_static/releases/test/index.html
-ln -sf /data/web_static/releases/test/ /data/web_static/current
-chown -R ubuntu /data/
-chgrp -R ubuntu /data/
 
+# Creates a fake HTML file /data/web_static/releases/test/index.html (with simple content, to test your Nginx configuration)
+printf %s "<html>
+	<head>
+	</head>
+	<body>
+		Holberton School
+	</body>
+</html>
+" > /data/web_static/releases/test/index.html
+# Creates a symbolic link /data/web_static/current linked to the /data/web_static/releases/test/ folder. If the symbolic link already exists, it should be deleted and recreated every time the script is ran.
+ln -sf /data/web_static/releases/test/ /data/web_static/current
+
+# Give ownership of the /data/ folder to the ubuntu user AND group (you can assume this user and group exist). This should be recursive; everything inside should be created/owned by this user/group.
+chown -hR ubuntu:ubuntu /data/
+chmod -R 755 /data/
+
+# Update the Nginx configuration to serve the content of /data/web_static/current/ to hbnb_static (ex: https://mydomainname.tech/hbnb_static).
 printf %s "server {
 	listen 80 default_server;
 	listen [::]:80 default_server;
@@ -21,7 +40,6 @@ printf %s "server {
 
 	location /hbnb_static {
 		alias /data/web_static/current;
-		index index.html index.htm;
 	}
 
 	location /redirect_me {
@@ -34,4 +52,6 @@ printf %s "server {
 		internal;
 	}
 }" > /etc/nginx/sites-available/default
+
+# Restart Nginx after updating the configuration
 service nginx restart
