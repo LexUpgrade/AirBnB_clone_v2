df847be12f646a88fa1e188167e026b89e7646c9
LexUpgrade <alexanderedim89@gmail.com>
2024-07-24 16:20:16 +0100

    Configures a web server for deployment of web_static

--- 101-setup_web_static.pp
+++ 101-setup_web_static.pp
@@ -8,7 +8,7 @@ $nginx_conf = "server {
 	root   /var/www/html;
 	index  index.html index.htm;
 
-	add_header X-Served-By $HOSTNAME
+	add_header X-Served-By ${hostname};
 	location /hbnb_static {
 		alias /data/web_static/current;
 	}
@@ -37,37 +37,37 @@ $html = "<html>
 package { 'nginx':
   ensure   => 'present',
   provider => 'apt'
-} ->
+}
 
 file { '/data':
   ensure  => 'directory'
-} ->
+}
 
 file { '/data/web_static':
   ensure => 'directory'
-} ->
+}
 
 file { '/data/web_static/releases':
   ensure => 'directory'
-} ->
+}
 
 file { '/data/web_static/releases/test':
   ensure => 'directory'
-} ->
+}
 
 file { '/data/web_static/shared':
   ensure => 'directory'
-} ->
+}
 
 file { '/data/web_static/releases/test/index.html':
   ensure  => 'present',
   content => $html
-} ->
+}
 
 file { '/data/web_static/current':
   ensure => 'link',
   target => '/data/web_static/releases/test'
-} ->
+}
 
 exec { 'chown -R ubuntu:ubuntu /data/':
   path => '/usr/bin/:/usr/local/bin/:/bin/'
@@ -75,26 +75,26 @@ exec { 'chown -R ubuntu:ubuntu /data/':
 
 file { '/var/www':
   ensure => 'directory'
-} ->
+}
 
 file { '/var/www/html':
   ensure => 'directory'
-} ->
+}
 
 file { '/var/www/html/index.html':
   ensure  => 'present',
   content => "Hello World!\n"
-} ->
+}
 
 file { '/var/www/html/404.html':
   ensure  => 'present',
   content => "Ceci n'est pas une page\n"
-} ->
+}
 
 file { '/etc/nginx/sites-available/default':
   ensure  => 'present',
   content => $nginx_conf
-} ->
+}
 
 exec { 'nginx restart':
   path => '/etc/init.d/'
