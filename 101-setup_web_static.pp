ad03e837828009b2b7f10f4ab8d44d5a9bf20fe0
LexUpgrade <alexanderedim89@gmail.com>
2024-07-24 17:24:36 +0100

    Configures a web server for deployment of web_static

--- 101-setup_web_static.pp
+++ 101-setup_web_static.pp
@@ -1,11 +1,10 @@
 # Configures a web server for deployment of web_static.
 
 # Nginx configuration file
-$nginx_conf = @("EOF")
-server {
+$nginx_conf = "server {
 	listen 80 default_server;
 	listen [::]:80 default_server;
-	
+
 	root   /var/www/html;
 	index  index.html index.htm;
 
@@ -15,7 +14,7 @@ server {
 	}
 
 	location /redirect_me {
-		return 301 http://cuberule.com/;
+		return 301 https://www.youtube.com/watch?v=QH2-TGUlwu4;
 	}
 
 	error_page 404 /404.html;
@@ -23,112 +22,80 @@ server {
 		root /var/www/html;
 		internal;
 	}
-}
-EOF
+}"
 
 # Fake HTML file
-$html = @("EOF")
-<html>
-	<head>
-	</head>
-	<body>
-		Holberton School
-	</body>
+$html = "<html>
+        <head>
+        </head>
+        <body>
+                Holberton School
+        </body>
 </html>
-EOF
+"
 
 package { 'nginx':
   ensure   => 'present',
-  provider => 'apt',
+  provider => 'apt'
 }
 
 file { '/data':
-  ensure  => 'directory',
-  owner   => 'ubuntu',
-  group   => 'ubuntu',
+  ensure  => 'directory'
 }
 
 file { '/data/web_static':
-  ensure  => 'directory',
-  owner   => 'ubuntu',
-  group   => 'ubuntu',
-  require => File['/data']
+  ensure => 'directory'
 }
 
 file { '/data/web_static/releases':
-  ensure  => 'directory',
-  owner   => 'ubuntu',
-  group   => 'ubuntu',
-  require => File['/data/web_static']
+  ensure => 'directory'
 }
 
 file { '/data/web_static/releases/test':
-  ensure  => 'directory',
-  owner   => 'ubuntu',
-  group   => 'ubuntu',
-  require => File['/data/web_static/releases']
+  ensure => 'directory'
 }
 
 file { '/data/web_static/shared':
-  ensure  => 'directory',
-  owner   => 'ubuntu',
-  group   => 'ubuntu',
-  require => File['/data/web_static/releases/test']
+  ensure => 'directory'
 }
 
 file { '/data/web_static/releases/test/index.html':
   ensure  => 'present',
-  content => $html,
-  owner   => 'ubuntu',
-  group   => 'ubuntu',
-  require => File['/data/web_static/shared']
+  content => $html
 }
 
 file { '/data/web_static/current':
-  ensure  => 'link',
-  target  => '/data/web_static/releases/test',
-  require => File['/data/web_static/releases/test']
+  ensure => 'link',
+  target => '/data/web_static/releases/test'
+}
+
+exec { 'chown -R ubuntu:ubuntu /data/':
+  path => '/usr/bin/:/usr/local/bin/:/bin/'
 }
 
 file { '/var/www':
-  ensure  => 'directory',
-  owner   => 'ubuntu',
-  group   => 'ubuntu',
+  ensure => 'directory'
 }
 
 file { '/var/www/html':
-  ensure  => 'directory',
-  owner   => 'ubuntu',
-  group   => 'ubuntu',
-  require => File['/var/www']
+  ensure => 'directory'
 }
 
 file { '/var/www/html/index.html':
   ensure  => 'present',
-  content => "Hello World!\n",
-  owner   => 'ubuntu',
-  group   => 'ubuntu',
-  require => File['/var/www/html']
+  content => "Hello World!\n"
 }
 
 file { '/var/www/html/404.html':
   ensure  => 'present',
-  content => "Ceci n'est pas une page\n",
-  owner   => 'ubuntu',
-  group   => 'ubuntu',
-  require => File['/var/www/html/index.html']
+  content => "Ceci n'est pas une page\n"
 }
 
 file { '/etc/nginx/sites-available/default':
   ensure  => 'present',
-  content => $nginx_conf,
-  owner   => 'root',
-  group   => 'root',
-  notify  => Service['nginx']
+  content => $nginx_conf
 }
 
-service { 'nginx':
-  ensure    => 'running',
-  enable    => true,
-  subscribe => File['/etc/nginx/sites-available/default'],
+exec { 'nginx restart':
+  path => '/etc/init.d/'
 }
