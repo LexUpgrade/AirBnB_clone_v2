ec04e84867011bd3e3c34bb80ec14064a8cbca1d
LexUpgrade <alexanderedim89@gmail.com>
2024-08-19 10:26:46 +0100

    Prints a list of states

---
+++ web_flask/7-states_list.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""Starts a Flask web application."""
+from flask import Flask, render_template
+from models import storage
+
+
+app = Flask(__name__)
+
+
+@app.route("/states_list", strict_slashes=False)
+def state_list():
+    """Displays a HTML page."""
+    states = storage.all("State").values()
+    return render_template("7-states_list.html", states=states)
+
+
+@app.teardown_appcontext
+def teardown(exc):
+    storage.close()
+
+
+if __name__ == "__main__":
+    """Starts the web application and set the host to '0.0.0.0'."""
+    app.run(host="0.0.0.0")
